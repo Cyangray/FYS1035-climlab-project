0c6fe1044eee84ac1e69644c056700a3ee839c25
Cyangray <54809768+Cyangray@users.noreply.github.com>
2021-11-17 20:54:21 +0100

    Better plots

--- plot_worst_scenario_reverse_2040-2054.py
+++ plot_worst_scenario_reverse_2040-2054.py
@@ -17,8 +17,8 @@ from climlib import *
 
 year_list = [2036, 2043, 2044, 2050]
 
-cmap = matplotlib.cm.get_cmap('Dark2')
-alpha = 0.7
+cmap = matplotlib.cm.get_cmap('Accent')
+alpha = 1
 fig, axs = plt.subplots(nrows = 1, ncols = 3, figsize=(15,7))
 plt.suptitle('Hva skjer hvis vi bestemmer oss for å reversere utslipp på et visst år?')
 axs.flatten()
@@ -30,18 +30,24 @@ for i, year_thresh in enumerate(reversed(year_list)):
     
     #plot icelats and co2e levels
     predmodel = predictions[3]
-    axs[0].plot(predmodel.years, predmodel.co2e_array, color = cmap(i/len(year_list)), linestyle = '-', label = 'Revers i %s'%year_thresh, alpha = alpha)
-    axs[1].plot(predmodel.co2e_array, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Revers i %s'%year_thresh, alpha = alpha)
-    axs[2].plot(predmodel.years, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Revers i %s'%year_thresh, alpha = alpha)
+    
     if year_thresh == 2044:
         axs[0].plot(predmodel.years, predmodel.co2e_array, color = cmap(i/len(year_list)), linestyle = '--', label = 'Revers i %s'%year_thresh, alpha = alpha)
         axs[1].plot(predmodel.co2e_array, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '--', label = 'Revers i %s'%year_thresh, alpha = alpha)
         axs[2].plot(predmodel.years, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '--', label = 'Revers i %s'%year_thresh, alpha = alpha)
+        
+        #axs[0].plot(predmodel.years[2050-2021], predmodel.co2e_array[2050-2021], 'ko')
+        axs[1].annotate(text = 'isfritt', xy=(predmodel.co2e_array[2060-2021], predmodel.icelat[2060-2021]), xytext=(predmodel.co2e_array[2060-2021], predmodel.icelat[2060-2021]-0.7))
+    else:
+        axs[0].plot(predmodel.years, predmodel.co2e_array, color = cmap(i/len(year_list)), linestyle = '-', label = 'Revers i %s'%year_thresh, alpha = alpha)
+        axs[1].plot(predmodel.co2e_array, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Revers i %s'%year_thresh, alpha = alpha)
+        axs[2].plot(predmodel.years, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Revers i %s'%year_thresh, alpha = alpha)
     
     
     max_co2_i = np.argmax(predmodel.co2e_array)
     axs[0].plot(predmodel.years[max_co2_i], predmodel.co2e_array[max_co2_i], 'ko')
     axs[0].annotate(text = int(year_thresh), xy=(predmodel.years[max_co2_i], predmodel.co2e_array[max_co2_i]), xytext=(predmodel.years[max_co2_i]+4, predmodel.co2e_array[max_co2_i]-3))
+    '''
     axs[1].plot(predmodel.co2e_array[max_co2_i], predmodel.icelat[max_co2_i], 'ko')
     if year_thresh > 2044:
         axs[1].annotate(text = int(year_thresh), xy=(predmodel.co2e_array[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.co2e_array[max_co2_i]+1, predmodel.icelat[max_co2_i]-0.5*((year_thresh%4) - 0.5)))
@@ -49,24 +55,25 @@ for i, year_thresh in enumerate(reversed(year_list)):
         axs[1].annotate(text = int(year_thresh), xy=(predmodel.co2e_array[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.co2e_array[max_co2_i]+7, predmodel.icelat[max_co2_i]-0.2))
     axs[2].plot(predmodel.years[max_co2_i], predmodel.icelat[max_co2_i], 'ko')
     axs[2].annotate(text = int(year_thresh), xy=(predmodel.years[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.years[max_co2_i]+4, predmodel.icelat[max_co2_i]-0.2))
-    
-    
-#axs[0].set_title('Cut year %s'%year_thresh)
+    '''
+
 axs[0].plot(predmodel.years[0], predmodel.co2e_array[0], 'ko')
 axs[1].plot(predmodel.co2e_array[0], predmodel.icelat[0], 'ko')
 axs[2].plot(predmodel.years[0], predmodel.icelat[0], 'ko')
-
-axs[0].annotate(text = 'Dagens nivå', xy=(predmodel.years[0], predmodel.co2e_array[0]), xytext=(predmodel.years[0]+4, predmodel.co2e_array[0]-3))
+axs[0].annotate(text = 'Dagens nivå', xy=(predmodel.years[0], predmodel.co2e_array[0]), xytext=(predmodel.years[0]-1, predmodel.co2e_array[0]-7))
 axs[1].annotate(text = 'Dagens nivå', xy=(predmodel.co2e_array[0], predmodel.icelat[0]), xytext=(predmodel.co2e_array[0]+7, predmodel.icelat[0]-0.2))
 axs[2].annotate(text = 'Dagens nivå', xy=(predmodel.years[0], predmodel.icelat[0]), xytext=(predmodel.years[0]+4, predmodel.icelat[0]-0.2))
+
+axs[0].set_xlim(2020,2165)
+axs[2].set_xlim(2020,2165)
 axs[1].set_ylim(70,91)
 #axs[0].set_ylim(420,630)
 axs[0].set_xlabel('År')
 axs[0].set_ylabel('CO2e-nivåer [ppm]')
 axs[1].set_xlabel('CO2e-nivåer [ppm]')
-axs[1].set_ylabel('Islinje (breddegrad)')
+axs[1].set_ylabel('Iskant [breddegrad]')
 axs[2].set_xlabel('År')
-axs[2].set_ylabel('Islinje (breddegrad)')
+axs[2].set_ylabel('Iskant [breddegrad]')
 axs[0].legend()
 axs[1].legend()
 axs[2].legend()
--- plot_worst_scenario_stop_2040-2054.py
+++ plot_worst_scenario_stop_2040-2054.py
@@ -16,58 +16,65 @@ from climlib import *
 
 #name = 'predicts.npy'
 
-year_list = [2036, 2043, 2050]
+year_list = [2036, 2043, 2044, 2050]
 
-cmap = matplotlib.cm.get_cmap('magma')
+cmap = matplotlib.cm.get_cmap('Accent')
 alpha = 0.7
 fig, axs = plt.subplots(nrows = 1, ncols = 3, figsize=(15,7))
 plt.suptitle('Hva skjer hvis vi bestemmer oss for å stoppe utslipp helt på et visst år?')
 axs.flatten()
 for i, year_thresh in enumerate(reversed(year_list)):
-    name = 'predicts_vippepunkt_s_%s.npy'%year_thresh
-    
-    #load prediction models
-    predictions = np.load(name, allow_pickle = True)
-    
-    #plot icelats and co2e levels
-    predmodel = predictions[3]
-    axs[0].plot(predmodel.years, predmodel.co2e_array, color = cmap(i/len(year_list)), linestyle = '-', label = 'Stopp i %s'%year_thresh, alpha = alpha)
-    axs[1].plot(predmodel.co2e_array, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Stopp i %s'%year_thresh, alpha = alpha)
-    axs[2].plot(predmodel.years, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Stopp i %s'%year_thresh, alpha = alpha)
     if year_thresh == 2044:
-        axs[0].plot(predmodel.years, predmodel.co2e_array, color = cmap(i/len(year_list)), linestyle = '--', label = 'Stopp i %s'%year_thresh, alpha = alpha)
-        axs[1].plot(predmodel.co2e_array, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '--', label = 'Stopp i %s'%year_thresh, alpha = alpha)
-        axs[2].plot(predmodel.years, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '--', label = 'Stopp i %s'%year_thresh, alpha = alpha)
-    
-    max_co2_i = np.argmax(predmodel.co2e_array)
-    axs[1].plot(predmodel.co2e_array[max_co2_i], predmodel.icelat[max_co2_i], 'ko')
-    if year_thresh > 2044:
-        axs[1].annotate(text = int(year_thresh), xy=(predmodel.co2e_array[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.co2e_array[max_co2_i]+1, predmodel.icelat[max_co2_i]-0.5*((year_thresh%4) - 0.5)))
+        pass
     else:
-        axs[1].annotate(text = int(year_thresh), xy=(predmodel.co2e_array[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.co2e_array[max_co2_i]+7, predmodel.icelat[max_co2_i]-0.2))
-    axs[0].plot(predmodel.years[max_co2_i], predmodel.co2e_array[max_co2_i], 'ko')
-    axs[0].annotate(text = int(year_thresh), xy=(predmodel.years[max_co2_i], predmodel.co2e_array[max_co2_i]), xytext=(predmodel.years[max_co2_i]+2, predmodel.co2e_array[max_co2_i]-7))
-    axs[2].plot(predmodel.years[max_co2_i], predmodel.icelat[max_co2_i], 'ko')
-    axs[2].annotate(text = int(year_thresh), xy=(predmodel.years[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.years[max_co2_i]+4, predmodel.icelat[max_co2_i]-0.2))
+        name = 'predicts_vippepunkt_s_%s.npy'%year_thresh
+        
+        #load prediction models
+        predictions = np.load(name, allow_pickle = True)
+        
+        #plot icelats and co2e levels
+        predmodel = predictions[3]
+        axs[0].plot(predmodel.years, predmodel.co2e_array, color = cmap(i/len(year_list)), linestyle = '-', label = 'Stopp i %s'%year_thresh, alpha = alpha)
+        axs[1].plot(predmodel.co2e_array, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Stopp i %s'%year_thresh, alpha = alpha)
+        axs[2].plot(predmodel.years, predmodel.icelat, color = cmap(i/len(year_list)), linestyle = '-', label = 'Stopp i %s'%year_thresh, alpha = alpha)
+        
+        
+        max_co2_i = np.argmax(predmodel.co2e_array)
+        if year_thresh == 2050:
+            axs[1].annotate(text = 'isfritt', xy=(predmodel.co2e_array[2047-2021], predmodel.icelat[2047-2021]), xytext=(predmodel.co2e_array[2047-2021]-20, predmodel.icelat[2047-2021]-0.7))
+        
+        
+        axs[0].plot(predmodel.years[max_co2_i], predmodel.co2e_array[max_co2_i], 'ko')
+        axs[0].annotate(text = int(year_thresh), xy=(predmodel.years[max_co2_i], predmodel.co2e_array[max_co2_i]), xytext=(predmodel.years[max_co2_i]+2, predmodel.co2e_array[max_co2_i]-7))
+        
+        axs[0].scatter(predmodel.years[-1], predmodel.co2e_array[-1], color = cmap(i/len(year_list)), linestyle = 'dotted')
+        axs[1].scatter(predmodel.co2e_array[-1], predmodel.icelat[-1], color = cmap(i/len(year_list)), linestyle = 'dotted')
+        axs[2].scatter(predmodel.years[-1], predmodel.icelat[-1], color = cmap(i/len(year_list)), linestyle = 'dotted')
+        #axs[1].annotate(text = int(year_thresh), xy=(predmodel.co2e_array[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.co2e_array[max_co2_i]+7, predmodel.icelat[max_co2_i]-0.2))
+        '''
+        axs[2].plot(predmodel.years[max_co2_i], predmodel.icelat[max_co2_i], 'ko')
+        axs[2].annotate(text = int(year_thresh), xy=(predmodel.years[max_co2_i], predmodel.icelat[max_co2_i]), xytext=(predmodel.years[max_co2_i]+4, predmodel.icelat[max_co2_i]-0.2))
+        '''
     
 #axs[1].set_title('Cut year %s'%year_thresh)
 axs[0].plot(predmodel.years[0], predmodel.co2e_array[0], 'ko')
 axs[1].plot(predmodel.co2e_array[0], predmodel.icelat[0], 'ko')
 axs[2].plot(predmodel.years[0], predmodel.icelat[0], 'ko')
-
 axs[0].annotate(text = 'Dagens nivå', xy=(predmodel.years[0], predmodel.co2e_array[0]), xytext=(predmodel.years[0]+4, predmodel.co2e_array[0]-3))
 axs[1].annotate(text = 'Dagens nivå', xy=(predmodel.co2e_array[0], predmodel.icelat[0]), xytext=(predmodel.co2e_array[0]+7, predmodel.icelat[0]-0.2))
 axs[2].annotate(text = 'Dagens nivå', xy=(predmodel.years[0], predmodel.icelat[0]), xytext=(predmodel.years[0]+4, predmodel.icelat[0]-0.2))
+
+axs[0].set_xlim(2020,2165)
+axs[2].set_xlim(2020,2165)
 axs[1].set_ylim(70,91)
-#axs[0].set_ylim(420,630)
 axs[0].set_xlabel('År')
 axs[0].set_ylabel('CO2e-nivåer [ppm]')
 axs[1].set_xlabel('CO2e-nivåer [ppm]')
-axs[1].set_ylabel('Islinje (breddegrad)')
+axs[1].set_ylabel('Iskant [breddegrad]')
 axs[2].set_xlabel('År')
-axs[2].set_ylabel('Islinje (breddegrad)')
+axs[2].set_ylabel('Iskant [breddegrad]')
 axs[0].legend()
-axs[1].legend()
+axs[1].legend(loc='lower right')
 axs[2].legend()
 axs[0].grid()
 axs[1].grid()
